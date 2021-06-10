1ed26728758596ea6520b98512ce5de523bd8327
Albert Millert <albertmillert97@gmail.com>
2021-06-11 01:03:42 +0200

    Rewrote `ScalaTest` tests as AnyFunSpec

--- src/test/scala/pl/amillert/nyctophobia/services/ConfigParserFlatSpecTest.scala
+++ src/test/scala/pl/amillert/nyctophobia/services/ConfigParserFlatSpecTest.scala
@@ -2,107 +2,147 @@ package pl.amillert
 package nyctophobia
 package services
 
-import org.scalatest.flatspec.AnyFlatSpec
+import org.scalatest.funspec.AnyFunSpec
 
-class ConfigParserFlatSpecTest extends AnyFlatSpec {
+class ConfigParserFlatSpecTest extends AnyFunSpec {
   import Setup._
   import errors._
 
-  behavior of "Config Parser"
+  describe("Config Parser") {
+    describe("when provided full list of arguments") {
+      describe("should succeed") {
+        describe(
+          "when two initial are valid directories' paths and the last one is threshold value in range [0, 100]"
+        ) {
+          assert(ConfigParser.eitherConfig(args) == Right(config))
+        }
+      }
 
-  it should "fail with NoParametersProvided error when not provided any arguments" in {
-    assert(
-      ConfigParser.eitherConfig(emptyList) == Left(
-        NoParametersProvided
-      )
-    )
-  }
+      describe("should fail") {
+        describe("when provided no arguments") {
+          describe("with `NoParametersProvided`") {
+            assert(
+              ConfigParser.eitherConfig(emptyList) == Left(
+                NoParametersProvided
+              )
+            )
+          }
+        }
+        describe("when not provided enough arguments") {
+          describe("with `WrongArityArguments(TooFew)`") {
+            describe("one argument") {
+              assert(
+                ConfigParser.eitherConfig(strListOneElement) == Left(
+                  WrongArityOfParameters(TooFew)
+                )
+              )
 
-  it should "fail with WrongArityArguments(TooFew) error when not provided enough arguments - one argument" in {
-    assert(
-      ConfigParser.eitherConfig(strListOneElement) == Left(
-        WrongArityOfParameters(TooFew)
-      )
-    )
-  }
+            }
+            describe("two arguments") {
+              assert(
+                ConfigParser.eitherConfig(strListTwoElements) == Left(
+                  WrongArityOfParameters(TooFew)
+                )
+              )
 
-  it should "fail with WrongArityArguments(TooFew) error when not provided enough arguments - two arguments" in {
-    assert(
-      ConfigParser.eitherConfig(strListTwoElements) == Left(
-        WrongArityOfParameters(TooFew)
-      )
-    )
-  }
+            }
+          }
+        }
 
-  it should "fail with WrongArityArguments(TooMany) error when not provided too many arguments - four arguments; wrong as suffix" in {
-    assert(
-      ConfigParser.eitherConfig(strListFourElementsSuffix) == Left(
-        WrongArityOfParameters(TooMany)
-      )
-    )
-  }
+        describe("when provided too many arguments") {
+          describe("with `WrongArityArguments(TooMany)`") {
+            describe("e.g. four arguments") {
+              describe("one additional provided as suffix") {
+                assert(
+                  ConfigParser.eitherConfig(
+                    strListFourElementsSuffix
+                  ) == Left(WrongArityOfParameters(TooMany))
+                )
+              }
+              describe("one additional provided as prefix") {
+                assert(
+                  ConfigParser.eitherConfig(
+                    strListFourElementsPrefix
+                  ) == Left(WrongArityOfParameters(TooMany))
+                )
+              }
+            }
+          }
+        }
 
-  it should "fail with WrongArityArguments(TooMany) error when not provided too many arguments - four arguments; wrong as prefix" in {
-    assert(
-      ConfigParser.eitherConfig(strListFourElementsPrefix) == Left(
-        WrongArityOfParameters(TooMany)
-      )
-    )
-  }
+        describe("when initial arguments are not valid directories") {
+          describe("with `NotADirectoryParameter`") {
+            describe("when the first argument is not a valid directory") {
+              assert(
+                ConfigParser.eitherConfig(argsWrongInDir) == Left(
+                  NotADirectoryParameter
+                )
+              )
+            }
+            describe("when the second argument is not a valid directory") {
+              assert(
+                ConfigParser.eitherConfig(argsWrongOutDir) == Left(
+                  NotADirectoryParameter
+                )
+              )
+            }
+            describe(
+              "when both first and second arguments are not valid directories"
+            ) {
+              assert(
+                ConfigParser.eitherConfig(argsWrongBothDirs) == Left(
+                  NotADirectoryParameter
+                )
+              )
+            }
+          }
 
-  it should "fail with NotADirectoryParameter error when either of the initial arguments is not a directory - first not a directory" in {
-    assert(
-      ConfigParser.eitherConfig(argsWrongInDir) == Left(
-        NotADirectoryParameter
-      )
-    )
-  }
+        }
 
-  it should "fail with NotADirectoryParameter error when either of the initial arguments is not a directory - second not a directory" in {
-    assert(
-      ConfigParser.eitherConfig(argsWrongOutDir) == Left(
-        NotADirectoryParameter
-      )
-    )
-  }
+        describe("when threshold argument can't be parsed to Int") {
+          describe("with `WrongThresholdValue`") {
+            describe("when Float provided instead") {
+              assert(
+                ConfigParser.eitherConfig(argsWrongThresholdFormat) == Left(
+                  WrongThresholdValue
+                )
+              )
+            }
+            describe("when random String provided instead") {
+              assert(
+                ConfigParser.eitherConfig(argsThresholdStr) == Left(
+                  WrongThresholdValue
+                )
+              )
+            }
+          }
+        }
 
-  it should "fail with NotADirectoryParameter error when either of the initial arguments is not a directory - both not directories" in {
-    assert(
-      ConfigParser.eitherConfig(argsWrongBothDirs) == Left(
-        NotADirectoryParameter
-      )
-    )
-  }
-
-  it should "fail with WrongThresholdValue error when the threshold argument can't be parsed to Int - Float as String" in {
-    assert(
-      ConfigParser.eitherConfig(argsWrongThresholdFormat) == Left(
-        WrongThresholdValue
-      )
-    )
-  }
-
-  it should "fail with WrongThresholdValue error when the threshold argument can't be parsed to Int - String value" in {
-    assert(
-      ConfigParser.eitherConfig(argsThresholdStr) == Left(
-        WrongThresholdValue
-      )
-    )
-  }
-
-  it should "fail with TooBigThresholdValue error when the threshold argument exceeds the range maximum value" in {
-    assert(
-      ConfigParser.eitherConfig(argsThresholdTooBig) == Left(
-        ThresholdNotInRange(TooBig)
-      )
-    )
-  }
-
-  it should "fail with TooBigThresholdValue error when the threshold argument below the range minimum value" in {
-    assert(
-      ConfigParser.eitherConfig(argsThresholdTooSmall) == Left(
-        ThresholdNotInRange(TooSmall)
-      )
-    )
+        describe("when threshold argument is not in range") {
+          describe("with `ThresholdNotInRange(TooBig)`") {
+            describe(
+              "when threshold argument exceeds the range maximum value"
+            ) {
+              assert(
+                ConfigParser.eitherConfig(argsThresholdTooBig) == Left(
+                  ThresholdNotInRange(TooBig)
+                )
+              )
+            }
+          }
+          describe("with `ThresholdNotInRange(TooSmall)`") {
+            describe(
+              "when threshold argument below the range minimum value"
+            ) {
+              assert(
+                ConfigParser.eitherConfig(argsThresholdTooSmall) == Left(
+                  ThresholdNotInRange(TooSmall)
+                )
+              )
+            }
+          }
+        }
+      }
+    }
   }
 }
