eed845179c1c25c3d48e7cb537f9e526cd3d8b0d
Albert Millert <albertmillert97@gmail.com>
2021-06-10 19:53:10 +0200

    Merged file procesing functions to a common object

---
+++ src/main/scala/pl/amillert/nyctophobia/services/FileManager.scala
@@ -0,0 +1,65 @@
+package pl.amillert
+package nyctophobia
+package services
+
+import java.io._
+import scala.util.Try
+import zio._
+
+trait FileManger
+
+object FileManager {
+  object FileReader {
+    import errors._
+
+    private type FileReaderEnv = FileReader.Service
+
+    private trait Service {
+      def read(config: Config): UIO[Array[File]]
+    }
+
+    private val live: FileReaderEnv = (config: Config) =>
+      ZIO.fromEither {
+        Try(
+          new File(config.inDir).listFiles.filter(_.isFile)
+        ).toEither match {
+          case Right(files: Array[File]) =>
+            Right(files)
+          case Left(_: java.lang.NullPointerException) =>
+            Left(LoadingFailedWrongPath)
+          case Left(_: Throwable) =>
+            Left(LoadingFailedUnknown)
+        }
+      }.orDie
+
+    def read(config: Config): UIO[Array[File]] =
+      live.read(config)
+  }
+
+  object FileWriter {
+    import zio.console._
+
+    private type FileWriterEnv = FileWriter.Service
+
+    private trait Service {
+      def write(
+          src: File,
+          dst: File
+        ): ZIO[Console, IOException, Unit]
+    }
+
+    private val live: FileWriterEnv = (src: File, dst: File) =>
+      ZIO.effectTotal {
+        new FileOutputStream(dst)
+          .getChannel
+          .transferFrom(
+            new FileInputStream(src).getChannel,
+            0,
+            Long.MaxValue
+          )
+      } *> putStrLn(s"[Info] Saved new file: ${dst.getName}")
+
+    def write(src: File, dst: File): ZIO[Console, IOException, Unit] =
+      live.write(src, dst)
+  }
+}
